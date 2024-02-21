9024027af8835bc6b6cf353d05c4e48e68b264f8
Andrew Porter <mephisto83@hotmail.com>
2024-02-20 19:30:49 -0600

    'deploying...'

--- types/aframe.d.ts
+++ types/aframe.d.ts
@@ -1,4 +1,11 @@
 // aframe.d.ts
+// In your a-frame-components.d.ts
+declare module 'a-frame-components' {
+    // Export all of your package's public methods, elements, and other members like it:
+    export async function load(): Promise<void>;
+    // This is sketchy and, in most surface, a short story of what your model/symbols say.
+}
+
 declare namespace JSX {
     interface IntrinsicElements {
         'a-base-interactive': {
