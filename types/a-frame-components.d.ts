22b09ed91097f114bf6fdc8c42b7252e5abe6f37
Andrew Porter <mephisto83@hotmail.com>
2024-02-20 19:36:44 -0600

    'deploying...'

--- types/a-frame-components.d.ts
+++
@@ -1,6 +0,0 @@
-// In your a-frame-components.d.ts
-declare module 'a-frame-components' {
-    // Export all of your package's public methods, elements, and other members like it:
-    export async function load(): Promise<void>;
-    // This is sketchy and, in most surface, a short story of what your model/symbols say.
-}
