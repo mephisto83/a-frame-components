dd88c2967581cc354372d265376f92bb95b25b09
Andrew Porter <mephisto83@hotmail.com>
2024-02-23 19:33:26 -0600

    'deploying...'

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import painter from "./react";
+import useEventListenerOn from "./react/useEventListenerOn";
+
+export { useEventListenerOn };
 export async function load() {
     return painter();
 }
