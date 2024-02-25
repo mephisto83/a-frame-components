e881588a0af547bc2ea153938873bc8bdeac77ef
Andrew Porter <mephisto83@hotmail.com>
2024-02-25 17:53:56 -0600

    'deploying...'

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import painter from "./react";
-import ui from "./react/systems/ui";
 import useEventListenerOn from "./react/useEventListenerOn";
 import { useBindEventOn } from './react/useEventListenerOn';
 
 export { useBindEventOn, useEventListenerOn };
 export async function load() {
-    ui();
     return painter();
 }
