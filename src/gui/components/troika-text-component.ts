dbd846908b2c5075fc8587a494ec31b3aabaa09a
Andrew Porter <mephisto83@hotmail.com>
2024-02-25 16:49:46 -0600

    'deploying...'

--- src/gui/components/troika-text-component.ts
+++ src/gui/components/troika-text-component.ts
@@ -140,7 +140,11 @@ export default function () {
                         caretPositions[caretPositions.length - 3]
                     let y = caretPositions[(this.caretIndex * 4) + 2] ??
                         caretPositions[(caretPositions.length - 3) + 2]
+                    let y2 = caretPositions[(this.caretIndex * 4) + 3] ??
+                        caretPositions[(caretPositions.length - 3) + 3]
+
                     this.textCursor.setAttribute('position', `${x || 0} ${y || 0} 0.02`);
+                    this.textCursor.setAttribute('scale', `${(y2 - y) * .1} .15 1`);
                 }
             }
         },
