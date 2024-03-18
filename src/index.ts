b734daa5970f3f1f0760521bfb2872a7d38439ec
Andrew Porter <mephisto83@hotmail.com>
2024-03-18 18:00:06 -0500

    'deploying...'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import painter from "./react";
 import useEventListenerOn from "./react/useEventListenerOn";
 import { raiseOn, setAttribute, useBindEventOn } from './react/useEventListenerOn';
-
-export { raiseOn, setAttribute, useBindEventOn, useEventListenerOn };
+import { createElement } from "./util";
+export { raiseOn, setAttribute, useBindEventOn, useEventListenerOn, createElement };
 export async function load() {
     return painter();
 }
--- types/a-frame-components.d.ts
+++ types/a-frame-components.d.ts
@@ -5,7 +5,7 @@ declare module 'a-frame-components' {
     export async function load(): Promise<void>;
 
     export function setAttribute(props: { [`frame-id`]: string }, name: string, value: any);
-
+    export function createElement(elementType: any, attributesDic: { [str: string]: any }): (moreAttr: { [str: string]: any }) => Element;
     export function useEventListenerOn(
         evtName: string,
         evtHandler: (event: Event, element: Element) => void,
