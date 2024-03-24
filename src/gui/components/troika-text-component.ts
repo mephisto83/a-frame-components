2b4990f415bccf8cf8e65ea3addf9d72661bbe4e
Andrew Porter <mephisto83@hotmail.com>
2024-03-24 14:07:58 -0500

    'deploying...'

--- src/gui/components/troika-text-component.ts
+++ src/gui/components/troika-text-component.ts
@@ -143,6 +143,11 @@ export default function () {
                     let scaleX = .025
                     let x_ = this.caretIndex * 4 + 1;
                     let y_ = this.caretIndex * 4 + 3;
+                    if (caretPositions.length === x_) {
+                        x_ = (this.caretIndex - 1) * 4 + 1;
+                        y_ = (this.caretIndex - 1) * 4 + 3;
+
+                    }
                     if (x_ < caretPositions.length && x_ >= 0 &&
                         y_ < caretPositions.length && y_ >= 0) {
                         let x = caretPositions[x_];
