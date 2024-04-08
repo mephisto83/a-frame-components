7db497a666d8eea7d02e7f5a1cc5462a80a9d9fb
Andrew Porter <mephisto83@hotmail.com>
2024-04-08 07:05:40 -0500

    'deploying...'

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import painter from "./react";
 import useEventListenerOn from "./react/useEventListenerOn";
-import { raiseOn, setAttribute, useEventsListenerOn, useBindEventOn } from './react/useEventListenerOn';
+import { raiseOn, setAttribute, useEventsListenerOn, findFrameElement, useBindEventOn } from './react/useEventListenerOn';
 import { createElement, findClosestValue } from "./util";
 import framemixin from './gui/components/mixin'
-export { framemixin, raiseOn, findClosestValue, setAttribute, useEventsListenerOn, useBindEventOn, useEventListenerOn, createElement };
+export { framemixin, raiseOn, findClosestValue, findFrameElement, setAttribute, useEventsListenerOn, useBindEventOn, useEventListenerOn, createElement };
 export async function load() {
     return painter();
 }
